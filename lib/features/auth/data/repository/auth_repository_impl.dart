890294db17465ac492e36fe62e242401861328fc
Hector Rauer <hector.rauer@gmail.com>
2024-03-27 16:37:37 -0300

    added domain signup repository and sign up usecase

---
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,35 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+  AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signupWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userID = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userID);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
